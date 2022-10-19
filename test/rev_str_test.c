b074b490a9d03dfe2ca822a4d2c5c28b7b71b043
kelvin <Kelvin>
2022-10-18 18:40:22 -0700

    running test files

---
+++ test/rev_str_test.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+	char *str;
+
+	str = "NULL";
+	a = _printf("%r\n", NULL);
+	printf("--->%d\n", a);
+
+	b = _printf("%r\n", str);
+	printf("%d\n", b);
+
+	b = _printf("%r\n", str);
+	printf("%d\n", b);
+	return (0);
+}
